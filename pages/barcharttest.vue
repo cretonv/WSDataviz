a37854ae22ef45698ff173b26df50bbfb56f287c
Anthony Lombard <Megathony@users.noreply.github.com>
2020-12-06 00:01:04 +0100

    Instalation du SCSS et Chart.js Ajout de mon tableau

---
+++ pages/barcharttest.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>BarChart</h1>
+    <BarChart> </BarChart>
+  </div>
+</template>
+
+<script>
+  import BarChart from "../components/graph/BarChart";
+
+  export default {
+    component() {
+      BarChart
+    }
+  }
+
+</script>
+
+<style lang="scss">
+  .hello {
+    color: red;
+
+    span {
+      color: blue;
+    }
+  }
+</style>
