64eb44fff8b6cad4032666807ccee5ac6fae8851
Vincent <vincecreton@hotmail.fr>
2020-12-08 15:10:26 +0100

    Intégration de la nouvelle page title

--- pages/index.vue
+++ pages/index.vue
@@ -14,7 +14,7 @@
           <div class="section">
             <RCfirst
                title="Catastrophes climatiques"
-               paragraph="Definition catastrophes climatiques"
+               paragraph="Lorem Ispum Lorem Ispum Lorem Ispum Lorem Ispum Lorem Ispum Lorem Ispum Lorem Ispum Lorem Ispum Lorem Ispum Lorem Ispum"
                page="01"
                imageSrc="/ressources/img/image1.jpg"
                imageXPosition="1/4"
