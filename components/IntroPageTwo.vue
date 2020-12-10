ee328ef33a82a454c9898ec576a9afd06fe6bbd7
Vincent <vincecreton@hotmail.fr>
2020-12-10 10:17:33 +0100

    Intégration partiel de la page 2 de l'intro

--- components/IntroPageTwo.vue
+++ components/IntroPageTwo.vue
@@ -2,6 +2,9 @@
   <div class="content">
     <div class="map">
     </div>
+    <div class="paragraph">
+      Les catastrophes climatiques n’épargnent pas la france.
+    </div>
   </div>
 </template>
 
@@ -29,6 +32,30 @@
       background-image: url("/ressources/img/svg/map_grise6.svg");
       background-size: contain;
       background-repeat: no-repeat;
+      opacity: 0.15;
+    }
+
+    .paragraph {
+      width: fit-content;
+      text-align: center;
+      margin: 48vh auto;
+      font-family: Montserrat;
+      font-style: normal;
+      font-weight: 600;
+      font-size: 24px;
+      line-height: 29px;
+      opacity: 0;
+      transition-delay: 1s;
+      transition: 1s all;
+    }
+    .active {
+      .fp-tableCell {
+        .paragraph {
+          opacity: 1;
+          transition: 2s all;
+          transition-delay: 0.5s;
+        }
+      }
     }
   }
 </style>
